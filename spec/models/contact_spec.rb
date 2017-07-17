ba71b0934977160aeea760d05a1bd8a4293eb8a0
shoken <syokenz@gmail.com>
2017-07-17 23:31:59 +0900

    Exercise Chapter.3 Model specs

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,6 +1,53 @@
 require 'rails_helper'
 
 describe Contact do
+  # 姓と名とメールがあれば有効な状態であること
+  it "is valid with a firstname, lastname and email" do
+    contact = Contact.new(
+      firstname: 'Shoken',
+      lastname: 'Fujisaki',
+      email: 'shoken@example.com')
+    expect(contact).to be_valid
+  end
+  # 名がなければ無効な状態であること
+  it "is invalid without a firstname" do
+    contact = Contact.new(firstname: nil)
+    contact.valid?
+    expect(contact.errors[:firstname]).to include("can't be blank")
+  end
+
+  # 姓がなければ無効な状態であること
+  it "is invalid without a lastname" do
+    contact = Contact.new(lastname: nil)
+    contact.valid?
+    expect(contact.errors[:lastname]).to include("can't be blank")
+  end
+  # メールアドレスがなければ無効な状態であること
+  it "is invalid without an email address" do
+    contact = Contact.new(email: nil)
+    contact.valid?
+    expect(contact.errors[:email]).to include("can't be blank")
+  end
+  # 重複したメールアドレスなら無効な状態であること
+  it "is invalid with a duplicate email address" do
+    Contact.create(
+      firstname: 'joe', lastname: 'tester',
+      email: 'tester@example.com'
+    )
+    contact = Contact.new(
+      firstname: 'jane', lastname: 'tester',
+      email: 'tester@example.com'
+    )
+    contact.valid?
+    expect(contact.errors[:email]).to include("has already been taken")
+  end
+  # 連絡先のフルネームを文字列として返すこと
+  it "returns a contact's full name as a string" do
+    contact = Contact.new(firstname: 'John', lastname: 'Doe',
+                          email: 'johndoe@example.com')
+    expect(contact.name).to eq 'John Doe'
+  end
+
   it "has a valid factory" do
     expect(build(:contact)).to be_valid
   end
